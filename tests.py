a59f8b4605e382f3c798973d0fddc40ad44cc5c7
cmyr <colin@cmyr.net>
2014-12-29 13:11:32 -0500

    added func to fetch nat_geo image, did some cleanup

--- tests.py
+++ tests.py
@@ -3,11 +3,9 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import requests
+import bs4
 
-sample_response = """<img id="result-img" src="../tmpfiles/20141227-18:46:44.jpg" height="300"/><h4>TAGS:</h4><h4>&nbsp;&nbsp;bridges&nbsp;&nbsp;wharf&nbsp;&nbsp;buidings&nbsp;&nbsp;bridge&nbsp;&nbsp;buildings&nbsp;&nbsp;</h4><br/><h4>Nearest Neighbor Sentence:</h4><ul><li>people walking on a bridge that has a small roof over it .</li></ul><br/><h4>Top-5 Generated:</h4><ul><li>a group of people walking down the buildings on a large bridge .</li><li>people are walking down a bridge .</li><li>two people walking down a bridge .</li><li>some buildings and people walking down a bridge .</li><li>four people are walking down a bridge .
-</li></ul>"""
-
-def raw_text_from_image(image_url):
+def response_for_image(image_url):
     base_url = 'http://deeplearning.cs.toronto.edu/api/url.php'
     files = {
     'urllink': ('', image_url),
@@ -17,25 +15,30 @@ def raw_text_from_image(image_url):
     return requests.post(base_url, files=files, headers=headers)
 
 
-    # http://graphics8.nytimes.com/images/2014/12/28/opinion/sunday/2014-yip-august-slide-0F09/2014-yip-august-slide-0F09-jumbo.jpg
-# ------WebKitFormBoundary5cTwys3Ujbn5yjUm
-# Content-Disposition: form-data; name="urllink"
+def description(raw_text):
+    soup = bs4.BeautifulSoup(raw_text, 'html.parser') 
+    return soup.li.get_text()
 
-# http://graphics8.nytimes.com/images/2014/12/28/opinion/sunday/2014-yip-february-slide-6MH0/2014-yip-february-slide-6MH0-jumbo.jpg
-# ------WebKitFormBoundary5cTwys3Ujbn5yjUm
-# Content-Disposition: form-data; name="url-2txt"
-
-# deeplearning.cs.toronto.edu resource-type-xhr   POST    HTTP    200     645 614 1.3471217155456543  10.155226945877075  0.0244600772857666
-# ------WebKitFormBoundary5cTwys3Ujbn5yjUm--
-
-
-
-
+def natgeo_pic_url():
+    source_url = 'http://photography.nationalgeographic.com/photography/photo-of-the-day/'
+    r = requests.get(source_url)
+    soup = bs4.BeautifulSoup(r.text)
+    for tag in soup.find_all('meta'):
+        if tag.has_attr('property'):
+            if tag['property'] == 'og:image':
+                return tag['content']
 
 def main():
-    test_url = 'http://graphics8.nytimes.com/images/2014/12/28/opinion/sunday/2014-yip-august-slide-0F09/2014-yip-august-slide-0F09-jumbo.jpg'
-    r = raw_text_from_image(test_url)
+    # descs = description(sample_response)
+    # print(descs)
+    # test_url = 'http://graphics8.nytimes.com/images/2014/12/28/opinion/sunday/2014-yip-august-slide-VNYR/2014-yip-august-slide-VNYR-jumbo.jpg'
+    # r = response_for_image(test_url)
+    # print(r.text)
+
+    pic_url = natgeo_pic_url()
+    r = response_for_image(pic_url)
     print(r.text)
 
+    # print(r.text)
 if __name__ == "__main__":
     main()

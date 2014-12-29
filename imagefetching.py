d80b4efa8d4aa3a4bf3753902cc01cbd47cc538b
cmyr <colin@cmyr.net>
2014-12-29 14:17:07 -0500

    added reuters imgs to imagefetching

--- imagefetching.py
+++ imagefetching.py
@@ -2,6 +2,10 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import requests
+import bs4
+import urllib
+import re
 
 def natgeo_pic_url():
     source_url = 'http://photography.nationalgeographic.com/photography/photo-of-the-day/'
@@ -12,9 +16,20 @@ def natgeo_pic_url():
             if tag['property'] == 'og:image':
                 return tag['content']
 
+def routers_imgs():
+    reuters = "http://www.reuters.com/news/pictures"
+    r = requests.get(reuters)
+    soup = bs4.BeautifulSoup(r.text)
+    images = [i for i in soup.find_all('img') 
+    if i.has_attr('alt') and i['alt'] == "Photo"]
+   
+    img_urls = [urllib.unquote(img['src']).decode('utf8') for img in images]
+    return [re.sub(r'w=300', r'w=620', i) for i in img_urls]
 
 def main():
-   pass
+    for i in routers_imgs():
+        print(i)
+
 
 if __name__ == "__main__":
     main()

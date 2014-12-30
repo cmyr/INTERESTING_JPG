fbb165327caa918ac4236d50cd3391d315a72e85
cmyr <colin@cmyr.net>
2014-12-30 12:41:49 -0500

    added trimmings.py, for defining ways of shortening caption

---
+++ trimmings.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+trimmings = [
+(' and ', ' & '),
+(r' \.', '.'),
+(' ,', ','),
+('people', 'ppl'),
+('person', 'prsn'),
+('group', 'grp')
+]

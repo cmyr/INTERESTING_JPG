0737809c344cbc144aa03a001ae64d85b286fea9
cmyr <colin@cmyr.net>
2014-12-30 12:06:06 -0500

    added func for getting 'editors choice' iamges from reuters

--- imagefetching.py
+++ imagefetching.py
@@ -42,19 +42,47 @@ def reuters_linked_images():
     for div in photo_divs:
         link = div.find('a')
         img = div.find('img', alt='Photo')
-
+        # we get a bunch of small versions, but we can resize them sneakily
+        if img:
+            img = re.sub(r'w=300', r'w=620', img.get('src'))    
+        
         if link and img:
             found_photos.append(
                 LinkedPhoto(reuters_base_url + link['href'], 
-                    urllib.unquote(img['src']).decode('utf8')
+                    urllib.unquote(img).decode('utf8')
                     )
                 )
     return found_photos
 
-def main():
-    for i in reuters_linked_images():
-        print(i)
+def reuters_slideshow_link():
+    reuters_base_url = "http://www.reuters.com"
+    reuters_photos_url = "http://www.reuters.com/news/pictures"
+    r = requests.get(reuters_photos_url)
+    soup = bs4.BeautifulSoup(r.text)
+    div = soup.find('div', class_='photo')
+    return reuters_base_url + div.find('a')['href']
 
+def reuters_slideshow_imgs():
+    link = reuters_slideshow_link()
+    r = requests.get(link)
+    soup = bs4.BeautifulSoup(r.text)
+    slides = soup.find_all('div', class_="slide")
+    print(slides)
+    imgs = list()
+    for slide in slides:
+        try:
+            img_link = slide.find('img')['data-lazy']
+            img_link = urllib.unquote(img_link).decode('utf8')
+            img_link = re.sub(r'&w=[0-9]+', r'&w=620', img_link)
+            imgs.append(LinkedPhoto(None, img_link))
+        except (KeyError, AttributeError, TypeError):
+            print('error extracting image from slide: %s' % slide.prettify())
+    return imgs
+
+
+def main():
+    imgs = reuters_slideshow_imgs()
+    print(imgs)
 
 if __name__ == "__main__":
     main()

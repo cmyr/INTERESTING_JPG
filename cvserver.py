0a9eb6ca3fb13a95e72141b71b987e9f7b2a8ab9
cmyr <colin@cmyr.net>
2015-01-07 11:47:21 -0500

    cleaned up cvserver, added top_caption helper function

--- cvserver.py
+++ cvserver.py
@@ -6,6 +6,7 @@ import requests
 import bs4
 import re
 
+
 def response_for_image(image_url):
     base_url = 'http://deeplearning.cs.toronto.edu/api/url.php'
     files = {
@@ -17,7 +18,7 @@ def response_for_image(image_url):
         'X-Requested-With': 'XMLHttpRequest',
         'User-agent': "@interesting_jpg %s v. 1.0" % self.name
         # 'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/600.2.5 (KHTML, like Gecko) Version/8.0.2 Safari/600.2.5"
-        }
+    }
     r = requests.post(base_url, files=files, headers=headers)
     text = r.text.strip()
     if not len(text):
@@ -26,7 +27,7 @@ def response_for_image(image_url):
     return text
 
 
-def description(raw_text):
+def nearest_neighbour(raw_text):
     if raw_text:
         soup = bs4.BeautifulSoup(raw_text, 'html.parser')
         try:
@@ -44,18 +45,16 @@ def captions(raw_text):
     return [c.text for c in captions]
 
 
+def top_caption(raw_text):
+    return captions(raw_text)[0]
+
 
 def main():
     sample_response = """<img id="result-img" src="../tmpfiles/20150107-10:35:13.jpg" height="300"/><h4>TAGS:</h4><h4>&nbsp;&nbsp;cycler&nbsp;&nbsp;peddler&nbsp;&nbsp;salesman&nbsp;&nbsp;rucksack&nbsp;&nbsp;pedicab&nbsp;&nbsp;</h4><br/><h4>Nearest Neighbor Sentence:</h4><ul><li>a woman outside with an umbrella riding a motor cart .</li></ul><br/><h4>Top-5 Generated:</h4><ul><li>two men are wearing a hat , riding on a bicycle with a backpack .</li><li>a man in a cart filled with bikes .</li><li>a man wearing a hat while trying to ride a bicycle on a bike .</li><li>a man riding a bicycle with a cart attached .</li><li>a man wearing a hat on a bicycle and carrying a cart .
     </li></ul>"""
 
-    captions(sample_response)
-    # soup = bs4.BeautifulSoup(sample_response)
-    # # print(soup.prettify())
-    # headers = soup.find('h4', text=re.compile(r'Top'))
-    # print(headers.find_next_sibling().li.string)
-    # print(type(headers))
-    # print([h.string for h in headers])
+    print("\n".join(captions(sample_response)))
+    print(top_caption(sample_response))
 
 
 if __name__ == "__main__":

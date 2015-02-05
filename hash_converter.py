cb70622bcb27ff3c17e3260c88778314375e07fe
cmyr <colin@cmyr.net>
2015-02-05 12:43:21 -0500

    added hash_converter utility

---
+++ hash_converter.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import shutil
+import imagehash
+
+def convert_hash_format(filepath):
+    shutil.copy(filepath, "%s.bak" % filepath)
+    to_convert = open(filepath).read().splitlines()
+    with open(filepath, 'w') as f:
+        for line in to_convert:
+                new_hash = imagehash.image_hash(line.strip())
+                if new_hash:
+                    f.write(str(new_hash)+"\n")
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=str, help="file to convert")
+    args = parser.parse_args()
+
+
+    if args.filename:
+        convert_hash_format(args.filename)
+
+
+if __name__ == "__main__":
+    main()
